34d6549b3095d45b94915985924d20231d89817b
Datamation-systems <dsplandroid@gmail.com>
2020-01-24 15:23:01 +0530

    Readjsonlist also done

--- app/src/main/java/com/datamation/hmdsfa/api/ApiInterface.java
+++ app/src/main/java/com/datamation/hmdsfa/api/ApiInterface.java
@@ -12,106 +12,106 @@ import retrofit2.http.Path;
 
 public interface ApiInterface {
 
-    @GET("fSalRep/mobile123/{dbname}/{macid}")
+    @GET("fSalRep/mobile123/{dbname}/{macid}")//01
     Call<ReadJsonList> getSalRepResult(@Path("dbname") String dbname,@Path("macid") String macid);
 
-    @GET("Fdebtor/mobile123/{dbname}/{repcode}")
+    @GET("Fdebtor/mobile123/{dbname}/{repcode}")//02
     Call<ReadJsonList> getDebtorResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fControl/mobile123/{dbname}")
+    @GET("fControl/mobile123/{dbname}")//03
     Call<ReadJsonList> getControlResult(@Path("dbname") String dbname);
 
-    @GET("fItemLoc/mobile123/{dbname}/{repcode}")
+    @GET("fItemLoc/mobile123/{dbname}/{repcode}")//04
     Call<ReadJsonList> getItemLocResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fItemPri/mobile123/{dbname}/{repcode}")
+    @GET("fItemPri/mobile123/{dbname}/{repcode}")//05
     Call<ReadJsonList> getItemPriResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fItems/mobile123/{dbname}/{repcode}")
+    @GET("fItems/mobile123/{dbname}/{repcode}")//06
     Call<ReadJsonList> getItemsResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fLocations/mobile123/{dbname}/{repcode}")
+    @GET("fLocations/mobile123/{dbname}/{repcode}")//07
     Call<ReadJsonList> getLocationsResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("Ftax/mobile123/{dbname}")
+    @GET("Ftax/mobile123/{dbname}")//08
     Call<ReadJsonList> getTaxResult(@Path("dbname") String dbname);
 
-    @GET("Ftaxhed/mobile123/{dbname}")
+    @GET("Ftaxhed/mobile123/{dbname}")//09
     Call<ReadJsonList> getTaxHedResult(@Path("dbname") String dbname);
 
-    @GET("Ftaxdet/mobile123/{dbname}")
+    @GET("Ftaxdet/mobile123/{dbname}")//10
     Call<ReadJsonList> getTaxDetResult(@Path("dbname") String dbname);
 
-    @GET("FnearDebtor/mobile123/{dbname}/{repcode}")
+    @GET("FnearDebtor/mobile123/{dbname}/{repcode}")//11
     Call<ReadJsonList> getNearDebtorResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("FCompanyBranch/mobile123/{dbname}/{repcode}")
+    @GET("FCompanyBranch/mobile123/{dbname}/{repcode}")//12
     Call<ReadJsonList> getCompanyBranchResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fCompanySetting/mobile123/{dbname}")
+    @GET("fCompanySetting/mobile123/{dbname}")//13
     Call<ReadJsonList> getCompanySettingResult(@Path("dbname") String dbname);
 
-    @GET("freason/mobile123/{dbname}")
+    @GET("freason/mobile123/{dbname}")//14
     Call<ReadJsonList> getReasonResult(@Path("dbname") String dbname);
 
-    @GET("fexpense/mobile123/{dbname}")
+    @GET("fexpense/mobile123/{dbname}")//15
     Call<ReadJsonList> getExpenseResult(@Path("dbname") String dbname);
 
-    @GET("fFreehed/mobile123/{dbname}/{repcode}")
+    @GET("fFreehed/mobile123/{dbname}/{repcode}")//16
     Call<ReadJsonList> getFreehedResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fFreeslab/mobile123/{dbname}")
+    @GET("fFreeslab/mobile123/{dbname}")//17
     Call<ReadJsonList> getFreeSlabResult(@Path("dbname") String dbname);
 
-    @GET("fFreedet/mobile123/{dbname}")
+    @GET("fFreedet/mobile123/{dbname}")//18
     Call<ReadJsonList> getFreeDetResult(@Path("dbname") String dbname);
 
-    @GET("fFreedeb/mobile123/{dbname}")
+    @GET("fFreedeb/mobile123/{dbname}")//19
     Call<ReadJsonList> getFreedebResult(@Path("dbname") String dbname);
 
-    @GET("ffreeitem/mobile123/{dbname}")
+    @GET("ffreeitem/mobile123/{dbname}")//20
     Call<ReadJsonList> getFreeitemResult(@Path("dbname") String dbname);
 
-    @GET("ffreemslab/mobile123/{dbname}")
+    @GET("ffreemslab/mobile123/{dbname}")//21
     Call<ReadJsonList> getFreeMSlabResult(@Path("dbname") String dbname);
 
-    @GET("fbank/mobile123/{dbname}")
+    @GET("fbank/mobile123/{dbname}")//22
     Call<ReadJsonList> getBankResult(@Path("dbname") String dbname);
 
-    @GET("fdisched/mobile123/{dbname}/{repcode}")
+    @GET("fdisched/mobile123/{dbname}/{repcode}")//23
     Call<ReadJsonList> getDiscHedResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fdiscdet/mobile123/{dbname}")
+    @GET("fdiscdet/mobile123/{dbname}")//24
     Call<ReadJsonList> getDiscDetResult(@Path("dbname") String dbname);
 
-    @GET("fdiscslab/mobile123/{dbname}")
+    @GET("fdiscslab/mobile123/{dbname}")//25
     Call<ReadJsonList> getDiscSlabResult(@Path("dbname") String dbname);
 
-    @GET("fdiscdeb/mobile123/{dbname}/{repcode}")
+    @GET("fdiscdeb/mobile123/{dbname}/{repcode}")//26
     Call<ReadJsonList> getDiscDebResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fTown/mobile123/{dbname}")
+    @GET("fTown/mobile123/{dbname}")//27
     Call<ReadJsonList> getTownResult(@Path("dbname") String dbname);
 
-    @GET("froute/mobile123/{dbname}/{repcode}")
+    @GET("froute/mobile123/{dbname}/{repcode}")//28
     Call<ReadJsonList> getRouteResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("froutedet/mobile123/{dbname}/{repcode}")
+    @GET("froutedet/mobile123/{dbname}/{repcode}")//29
     Call<ReadJsonList> getRouteDetResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("FItenrHed/mobile123/{dbname}/{repcode}")
+    @GET("FItenrHed/mobile123/{dbname}/{repcode}")//30
     Call<ReadJsonList> getItenrHedResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("FItenrDet/mobile123/{dbname}/{repcode}")
+    @GET("FItenrDet/mobile123/{dbname}/{repcode}")//31
     Call<ReadJsonList> getItenrDetResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("RepLastThreeInvDet/mobile123/{dbname}/{repcode}")
+    @GET("RepLastThreeInvDet/mobile123/{dbname}/{repcode}")//32
     Call<ReadJsonList> getLastThreeInvDetResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("RepLastThreeInvHed/mobile123/{dbname}/{repcode}")
+    @GET("RepLastThreeInvHed/mobile123/{dbname}/{repcode}")//33
     Call<ReadJsonList> getLastThreeInvHedResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
-    @GET("fDdbNoteWithCondition/mobile123/{dbname}/{repcode}")
+    @GET("fDdbNoteWithCondition/mobile123/{dbname}/{repcode}")//34
     Call<ReadJsonList> getOutstandingResult(@Path("dbname") String dbname,@Path("repcode") String repcode);
 
 }
--- app/src/main/java/com/datamation/hmdsfa/model/apimodel/ReadJsonList.java
+++ app/src/main/java/com/datamation/hmdsfa/model/apimodel/ReadJsonList.java
@@ -1,51 +1,243 @@
 package com.datamation.hmdsfa.model.apimodel;
 
+import com.datamation.hmdsfa.controller.TaxDetController;
 import com.datamation.hmdsfa.helpers.ListExpandHelper;
+import com.datamation.hmdsfa.model.Bank;
+import com.datamation.hmdsfa.model.CompanyBranch;
+import com.datamation.hmdsfa.model.CompanySetting;
 import com.datamation.hmdsfa.model.Control;
 import com.datamation.hmdsfa.model.Debtor;
+import com.datamation.hmdsfa.model.Discdeb;
+import com.datamation.hmdsfa.model.Discdet;
+import com.datamation.hmdsfa.model.Disched;
+import com.datamation.hmdsfa.model.Discslab;
+import com.datamation.hmdsfa.model.Expense;
+import com.datamation.hmdsfa.model.FInvhedL3;
+import com.datamation.hmdsfa.model.FItenrDet;
+import com.datamation.hmdsfa.model.FItenrHed;
+import com.datamation.hmdsfa.model.FddbNote;
+import com.datamation.hmdsfa.model.FinvDetL3;
+import com.datamation.hmdsfa.model.FreeDeb;
+import com.datamation.hmdsfa.model.FreeDet;
+import com.datamation.hmdsfa.model.FreeHed;
+import com.datamation.hmdsfa.model.FreeItem;
+import com.datamation.hmdsfa.model.FreeMslab;
+import com.datamation.hmdsfa.model.FreeSlab;
 import com.datamation.hmdsfa.model.Item;
 import com.datamation.hmdsfa.model.ItemLoc;
+import com.datamation.hmdsfa.model.Locations;
+import com.datamation.hmdsfa.model.NearDebtor;
+import com.datamation.hmdsfa.model.Reason;
+import com.datamation.hmdsfa.model.Route;
+import com.datamation.hmdsfa.model.RouteDet;
 import com.datamation.hmdsfa.model.SalRep;
+import com.datamation.hmdsfa.model.Tax;
+import com.datamation.hmdsfa.model.TaxDet;
+import com.datamation.hmdsfa.model.TaxHed;
+import com.datamation.hmdsfa.model.Town;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
 public class ReadJsonList {
 
-    @SerializedName("fSalRepResult")
+    @SerializedName("fSalRepResult")//01
     private List<SalRep> salRepResult = null;
+    @SerializedName("fControlResult")//02
+    private List<Control> controlResult = null;
+    @SerializedName("fItemLocResult")//03
+    private List<ItemLoc> itemLocResult = null;
+    @SerializedName("fItemPriResult")//05
+    private List<ItemLoc> itemPriResult = null ;
+    @SerializedName("fItemsResult")//06
+    private List<Item> itemsResult = null ;
+    @SerializedName("fLocationsResult")//07
+    private List<Locations> locationsResult = null ;
+    @SerializedName("fTaxResult")//08
+    private List<Tax> taxResult = null ;
+    @SerializedName("fTaxHedResult")//09
+    private List<TaxHed> taxHedResult = null ;
+    @SerializedName("fTaxDetResult")//10
+    private List<TaxDet> taxDetResult = null ;
+    @SerializedName("FnearDebtorResult")//11
+    private List<NearDebtor> nearDebtorResult = null ;
+    @SerializedName("FCompanyBranchResult")//12
+    private List<CompanyBranch> companyBranchResult = null ;
+    @SerializedName("fCompanySettingResult")//13
+    private List<CompanySetting> companySettingResult = null ;
+    @SerializedName("fReasonResult")//14
+    private List<Reason> reasonResult = null ;
+    @SerializedName("fExpenseResult")//15
+    private List<Expense> expenseResult = null ;
+    @SerializedName("FfreeslabResult")//16
+    private List<FreeSlab> freeSlabResult = null ;
+    @SerializedName("FfreedetResult")//17
+    private List<FreeDet> freeDetResult = null ;
+    @SerializedName("FfreedebResult")//18
+    private List<FreeDeb> freeDebResult = null ;
+    @SerializedName("fFreeItemResult")//19
+    private List<FreeItem> freeItemResult = null ;
+    @SerializedName("FfreehedResult")//20
+    private List<FreeHed> freeHedResult = null ;
+    @SerializedName("fFreeMslabResult")//21
+    private List<FreeMslab> freeMslabResult = null ;
+    @SerializedName("fbankResult")//22
+    private List<Bank> bankResult = null ;
+    @SerializedName("FdiscdetResult")//23
+    private List<Discdet> discDetResult = null ;
+    @SerializedName("FdiscslabResult")//24
+    private List<Discslab> discSlabResult = null ;
+    @SerializedName("FdiscdebResult")//25
+    private List<Discdeb> discDebResult = null ;
+    @SerializedName("FDischedResult")//26
+    private List<Disched> discHedResult = null ;
+    @SerializedName("fTownResult")//27
+    private List<Town> townResult = null ;
+    @SerializedName("fRouteResult")//28
+    private List<Route> routeResult = null ;
+    @SerializedName("fRouteDetResult")//29
+    private List<RouteDet> routeDetResult = null ;
+    @SerializedName("fItenrHedResult")//30
+    private List<FItenrHed> itenrHedResult = null ;
+    @SerializedName("fItenrDetResult")//31
+    private List<FItenrDet> itenrDetResult = null ;
+    @SerializedName("RepLastThreeInvDetResult")//32
+    private List<FinvDetL3> lastThreeInvDetResult = null ;
+    @SerializedName("RepLastThreeInvHedResult")//33
+    private List<FInvhedL3> lastThreeInvHedResult = null ;
+    @SerializedName("fDdbNoteWithConditionResult")//34
+    private List<FddbNote> outstandingResult = null ;
 
     public List<SalRep> getSalRepResult() {
         return salRepResult;
     }
 
-    @SerializedName("fControlResult")
-    private List<Control> controlResult = null;
-
     public List<Control> getControlResult() {
         return controlResult;
     }
-
-    @SerializedName("fItemLocResult")
-    private List<ItemLoc> itemLocResult = null;
 
     public List<ItemLoc> getItemLocResult() {
         return itemLocResult;
     }
 
-    @SerializedName("fItemPriResult")
-    private List<ItemLoc> itemPriResult = null ;
-
     public List<ItemLoc> getItemPriResult() {
         return itemPriResult;
     }
-
-    @SerializedName("fItemsResult")
-    private List<Item> itemsResult = null ;
 
     public List<Item> getItemsResult() {
         return itemsResult;
     }
 
+    public List<Locations> getLocationsResult() {
+        return locationsResult;
+    }
+
+    public List<Tax> getTaxResult() {
+        return taxResult;
+    }
+
+    public List<TaxHed> getTaxHedResult() {
+        return taxHedResult;
+    }
+
+    public List<TaxDet> getTaxDetResult() {
+        return taxDetResult;
+    }
+
+    public List<NearDebtor> getNearDebtorResult() {
+        return nearDebtorResult;
+    }
+
+    public List<CompanyBranch> getCompanyBranchResult() {
+        return companyBranchResult;
+    }
+
+    public List<CompanySetting> getCompanySettingResult() {
+        return companySettingResult;
+    }
+
+    public List<Reason> getReasonResult() {
+        return reasonResult;
+    }
+
+    public List<Expense> getExpenseResult() {
+        return expenseResult;
+    }
+
+    public List<FreeSlab> getFreeSlabResult() {
+        return freeSlabResult;
+    }
+
+    public List<FreeDet> getFreeDetResult() {
+        return freeDetResult;
+    }
+
+    public List<FreeDeb> getFreeDebResult() {
+        return freeDebResult;
+    }
+
+    public List<FreeItem> getFreeItemResult() {
+        return freeItemResult;
+    }
+
+    public List<FreeHed> getFreeHedResult() {
+        return freeHedResult;
+    }
+
+    public List<FreeMslab> getFreeMslabResult() {
+        return freeMslabResult;
+    }
+
+    public List<Bank> getBankResult() {
+        return bankResult;
+    }
+
+    public List<Discdet> getDiscDetResult() {
+        return discDetResult;
+    }
+
+    public List<Discslab> getDiscSlabResult() {
+        return discSlabResult;
+    }
+
+    public List<Discdeb> getDiscDebResult() {
+        return discDebResult;
+    }
+
+    public List<Disched> getDiscHedResult() {
+        return discHedResult;
+    }
+
+    public List<Town> getTownResult() {
+        return townResult;
+    }
+
+    public List<Route> getRouteResult() {
+        return routeResult;
+    }
+
+    public List<RouteDet> getRouteDetResult() {
+        return routeDetResult;
+    }
+
+    public List<FItenrHed> getItenrHedResult() {
+        return itenrHedResult;
+    }
+
+    public List<FItenrDet> getItenrDetResult() {
+        return itenrDetResult;
+    }
+
+    public List<FinvDetL3> getLastThreeInvDetResult() {
+        return lastThreeInvDetResult;
+    }
+
+    public List<FInvhedL3> getLastThreeInvHedResult() {
+        return lastThreeInvHedResult;
+    }
+
+    public List<FddbNote> getOutstandingResult() {
+        return outstandingResult;
+    }
 }
 
