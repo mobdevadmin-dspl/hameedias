5e52d9641d98907a8f409df8fd13ed234a2fc14a
MayumiR <mayumikarashmi@gmail.com>
2020-06-26 18:39:50 +0530

    print completed,checkbox removed

--- app/src/main/java/com/datamation/hmdsfa/dialog/VanSalePrintPreviewAlertBox.java
+++ app/src/main/java/com/datamation/hmdsfa/dialog/VanSalePrintPreviewAlertBox.java
@@ -593,13 +593,13 @@ public class VanSalePrintPreviewAlertBox {
 
     public void PrintCurrentview() {
          checkPrinter();
-//        if (PRINTER_MAC_ID.equals("404")) {
-//        Log.v("", "No MAC Address Found.Enter Printer MAC Address.");
-//        Toast.makeText(context, "No MAC Address Found.Enter Printer MAC Address.", Toast.LENGTH_LONG).show();
-//        }
-//        else {
-        printItems();
-   //0      }
+        if (PRINTER_MAC_ID.equals("404")) {
+        Log.v("", "No MAC Address Found.Enter Printer MAC Address.");
+        Toast.makeText(context, "No MAC Address Found.Enter Printer MAC Address.", Toast.LENGTH_LONG).show();
+        }
+       else {
+         printItems();
+        }
     }
 
 	/*-*-*-*--*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*--*-*-*-*-*-*-*-*/
