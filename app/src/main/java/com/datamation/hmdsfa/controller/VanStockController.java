226868202b243bd8134eac6c0a705c32697969ab
Kaveesha <k.kaveesha95@gmail.com>
2021-03-31 14:57:35 +0530

    get van stock by group wise in stock report

--- app/src/main/java/com/datamation/hmdsfa/controller/VanStockController.java
+++ app/src/main/java/com/datamation/hmdsfa/controller/VanStockController.java
@@ -318,7 +318,8 @@ public class VanStockController {
         Cursor cursor = null;
         try {
 
-            String selectQuery = "Select sum(v.Quantity_Issued) as totQty, itm.itemName as Description FROM fVanStock v, fItem itm WHERE v.To_Location_Code = '"+ locCode + "' AND v.Item_No = itm.itemCode GROUP BY GroupCode";
+            String selectQuery = "Select sum(v.Quantity_Issued) as totQty, itm.ItemName as Description FROM fVanStock v, fItem itm WHERE v.Item_No = itm.ItemCode AND v.To_Location_Code = '"+ locCode + "' GROUP BY itm.GroupCode";
+
 
             cursor = dB.rawQuery(selectQuery, null);
 
